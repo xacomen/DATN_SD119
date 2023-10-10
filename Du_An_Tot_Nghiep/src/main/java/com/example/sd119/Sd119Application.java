d22f0b9381c7220faa290f79693db68e698f460c
xacomen <minhhieunguyentrong@gmail.com>
2023-10-10 21:25:44 +0700

    DATN

---
+++ Du_An_Tot_Nghiep/src/main/java/com/example/sd119/Sd119Application.java
@@ -0,0 +1,13 @@
+package com.example.sd119;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sd119Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sd119Application.class, args);
+    }
+
+}
---
+++ Du_An_Tot_Nghiep/src/test/java/com/example/sd119/Sd119ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sd119;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sd119ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
